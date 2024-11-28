73fd6a463066f596fa5bd04615c2d2f3ba101b80
Makoto Inoue <2630+makoto@users.noreply.github.com>
2024-11-28 15:47:52 +0900

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,50 @@ import { useEffect, useState } from 'react';
 import { useAccount, useConnect, useDisconnect } from 'wagmi'
 import { useWalletClient } from 'wagmi'
 import {
+	getClient,
+	getAccount,
+  getEnableSessionDetails,
+  getSmartSessionsValidator
+} from '@rhinestone/module-sdk'
+import {
   getPublicClient,
   getSmartAccountClient,
   getSafeAccount,
+  getPimlicoClient,
+  setTrustAttesters,
+  installSmartSession,
+  getSession,
+  getSessionOwner,
+  createUserOperation,
+  signSmartSession,
+  executeUserOperation
 } from "./client.tsx"
-const publicClient = getPublicClient()
 
+const publicClient = getPublicClient()
+const sessionOwner = getSessionOwner()
+const session = getSession(sessionOwner)
+const smartSessions = getSmartSessionsValidator({})
 function App() {
   const [safeAccount, setSafeAccount] = useState();
   const [smartAccountClient, setSmartAccountClient] = useState();
   const [txHash, setTxHash] = useState();
+  const [trustedAttesterTxHash, setTrustedAttesterTxHash] = useState();
+  const [smartSessionTxHash, setSmartSessionTxHash] = useState();
+  const [sessionDetails, setSessionDetails] = useState();
+  const [userOperation, setUserOperation] = useState();
+  const [executeUserOperationTxHash, setExecuteUserOperationTxHash] = useState();
+  
   const account = useAccount()
   const { connectors, connect, status, error } = useConnect()
   const { disconnect } = useDisconnect()
-
+  const pimlicoClient = getPimlicoClient()
   const {data: walletClient} = useWalletClient();
+  // const client = getClient({
+  //   rpcUrl,
+  // })       
+  console.log({sessionOwner})
+  const client = getClient({rpcUrl:"https://rpc.ankr.com/eth_sepolia" })       
+
 
   const fetchData = async () => {
     try {
@@ -27,6 +56,19 @@ function App() {
       const _smartAccountClient = await getSmartAccountClient(_safeAccount)
       console.log('**fetchData3', {_smartAccountClient});
       setSmartAccountClient(_smartAccountClient)
+      
+      console.log('**fetchData4', {session});
+      const account = getAccount({
+        address: _safeAccount.address,
+        type: 'safe',
+      })
+      console.log('**fetchData5', {account, client, publicClient})
+      const sessionDetails = await getEnableSessionDetails({
+        sessions: [session],
+        account,
+        clients: [publicClient],
+      })
+      console.log('***fetchData6', {sessionDetails})
     } 
     catch (error:any) {
       console.log('**fetchData:error', error);
@@ -35,8 +77,8 @@ function App() {
 
   useEffect(() => {
     fetchData()
-    
   }, [walletClient]); // Empty dependency array means it runs only once when the component mounts
+
 
   const sendTx = (_smartAccountClient) => {
     console.log(1, _smartAccountClient)
@@ -49,11 +91,26 @@ function App() {
     })
   }
 
-  console.log(10, {
-    walletClient,
-    smartAccountClient,
-    safeAccount
-  })
+  const sendSetTrustAttestersTx = (
+    safeAccount: any, smartAccountClient: any, pimlicoClient: any
+  ) => {
+    setTrustAttesters(safeAccount,smartAccountClient,pimlicoClient)
+    .then((_tx:any) =>  {
+      setTrustedAttesterTxHash(_tx.receipt.transactionHash)
+    })
+  }
+
+  const sendInstallSmartSessionTx = (
+    smartAccountClient:any, pimlicoClient:any
+  ) => {
+    console.log('***1111', smartAccountClient, pimlicoClient)
+    installSmartSession(smartAccountClient, pimlicoClient)
+    .then((_tx:any) =>  {
+      console.log('***1112', _tx)
+      setSmartSessionTxHash(_tx.receipt.transactionHash)
+    })
+  }
+
   return (
     <>
       <div>
@@ -74,7 +131,9 @@ function App() {
         )}
         {safeAccount && (
           <div>
-            safe adress: { safeAccount.address}
+            <h5>
+              safe adress: { safeAccount.address}
+            </h5>
             <button type="button" onClick={() => sendTx(smartAccountClient)}>
               Send Tx
             </button>
@@ -83,6 +142,66 @@ function App() {
                 `https://sepolia.etherscan.io/tx/${txHash}`
               )
             }
+            <button type="button" onClick={() => sendSetTrustAttestersTx(safeAccount, smartAccountClient, pimlicoClient )}>
+              Set trusted attesters
+            </button>
+            {
+              trustedAttesterTxHash && (
+                `https://sepolia.etherscan.io/tx/${trustedAttesterTxHash}`
+              )
+            }
+            <button type="button" onClick={() => sendInstallSmartSessionTx(smartAccountClient, pimlicoClient, smartSessions )}>
+              Install smart session
+            </button>
+            {
+              smartSessionTxHash && (
+                `https://sepolia.etherscan.io/tx/${smartSessionTxHash}`
+              )
+            }
+
+            <button type="button" onClick={() => {
+              console.log('***', {safeAccount, walletClient})
+              signSmartSession(safeAccount, walletClient, session ).then(_sessionDetails => { 
+                console.log('****', {_sessionDetails})
+                setSessionDetails(_sessionDetails)
+              })
+            }
+            }>
+              Sign smart session
+            </button>
+
+            <button type="button" onClick={() => {
+              createUserOperation(
+                sessionOwner,
+                safeAccount,
+                walletClient,
+                publicClient,
+                smartAccountClient,
+                smartSessions,
+                sessionDetails,
+                session
+              ).then(_userOperation => {
+                setUserOperation(_userOperation)
+              })
+            }
+            }>
+              Create User Operation
+            </button>
+            <button type="button" onClick={() => {
+              executeUserOperation(smartAccountClient, pimlicoClient, userOperation)
+              .then((_txhash)=>{
+                console.log({_txhash})
+                setExecuteUserOperationTxHash(_txhash.receipt.transactionHash)
+              })
+            }}>
+              Execute user Operation
+            </button>
+            {
+              executeUserOperationTxHash && (
+                `https://sepolia.etherscan.io/tx/${executeUserOperationTxHash}`
+              )
+            }
+
           </div>
         )}
 
--- src/client.tsx
+++ src/client.tsx
@@ -1,35 +1,35 @@
-import { Address, createPublicClient, http } from "viem";
+import { Address, createPublicClient, http, toBytes, toFunctionSelector, toHex } from "viem";
 import { sepolia } from "viem/chains";
 import { createPimlicoClient } from "permissionless/clients/pimlico";
 import {
-  createPaymasterClient,
   entryPoint07Address,
+  getUserOperationHash
 } from "viem/account-abstraction";
 import { toSafeSmartAccount } from "permissionless/accounts";
 import { createSmartAccountClient } from "permissionless";
-// import { erc7579Actions } from "permissionless/actions/erc7579";
-// import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+import { getAccountNonce } from "permissionless/actions";
+import { erc7579Actions } from "permissionless/actions/erc7579";
+import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
+  
 import {
-	// getSmartSessionsValidator,
-	// OWNABLE_VALIDATOR_ADDRESS,
-	// getSudoPolicy,
-	// Session,
-	// getClient,
-	// getAccount,
-	// encodeSmartSessionSignature,
-	// getOwnableValidatorMockSignature,
+	getSmartSessionsValidator,
+	OWNABLE_VALIDATOR_ADDRESS,
+	getSudoPolicy,
+	Session,
+	getAccount,
+	encodeSmartSessionSignature,
+	getOwnableValidatorMockSignature,
 	RHINESTONE_ATTESTER_ADDRESS,
 	MOCK_ATTESTER_ADDRESS,
 	getTrustAttestersAction,
-	// encodeValidatorNonce,
-	// getOwnableValidator,
-	// encodeValidationData,
-	// getEnableSessionDetails,
-  } from '@rhinestone/module-sdk'
+	encodeValidatorNonce,
+	getOwnableValidator,
+	encodeValidationData,
+	getEnableSessionDetails,
+} from '@rhinestone/module-sdk'
   
 const rpcUrl = "https://rpc.ankr.com/eth_sepolia"
 const pimlicoUrl = `https://api.pimlico.io/v2/sepolia/rpc?apikey=${import.meta.env.VITE_PIMLICO_API_KEY}`
-export const paymasterUrl = "http://localhost:3000";
 
 export const getPublicClient = () => {
   return createPublicClient({
@@ -48,6 +48,10 @@ export const getPimlicoClient = () => {
   });
 };
 
+export const getSessionOwner = () => {
+    return  privateKeyToAccount(generatePrivateKey())
+}
+
 export const getTrustAttestersActionWithAttesters = () => {
     return getTrustAttestersAction({
         threshold: 1,
@@ -59,6 +63,11 @@ export const getTrustAttestersActionWithAttesters = () => {
 } 
 
 export const getSafeAccount = (publicClient:any,walletClient:any) => {
+    const ownableValidator = getOwnableValidator({
+        owners: [walletClient.account.address],
+        threshold: 1,
+    })
+
     return toSafeSmartAccount({
         // address: accountAddress,
         client: publicClient,
@@ -68,8 +77,19 @@ export const getSafeAccount = (publicClient:any,walletClient:any) => {
           address: entryPoint07Address,
           version: "0.7",
         },
-        // safe4337ModuleAddress: "0x7579EE8307284F293B1927136486880611F20002",
-        // erc7579LaunchpadAddress: "0x7579011aB74c46090561ea277Ba79D510c6C00ff",
+        safe4337ModuleAddress: "0x7579EE8307284F293B1927136486880611F20002",
+        erc7579LaunchpadAddress: "0x7579011aB74c46090561ea277Ba79D510c6C00ff",
+        attesters: [
+            RHINESTONE_ATTESTER_ADDRESS, // Rhinestone Attester
+            MOCK_ATTESTER_ADDRESS, // Mock Attester - do not use in production
+          ],
+          attestersThreshold: 1,
+          validators: [
+            {
+              address: ownableValidator.address,
+              context: ownableValidator.initData,
+            },
+          ],
     });
 };
   
@@ -77,12 +97,6 @@ export const getSmartAccountClient = async (
   safeAccount: any
 ) => {
   const pimlicoClient = getPimlicoClient()
-  console.log('***getSmartAccountClient', {
-    account: safeAccount,
-    chain: sepolia,
-    bundlerTransport: http(pimlicoUrl),
-    paymaster: pimlicoClient,
-  })
   return createSmartAccountClient({
     account: safeAccount,
     chain: sepolia,
@@ -93,6 +107,173 @@ export const getSmartAccountClient = async (
         return (await pimlicoClient.getUserOperationGasPrice()).fast;
       },
     },
-  });
-//   .extend(erc7579Actions());
+  }).extend(erc7579Actions());
 };
+
+
+export const setTrustAttesters = async (
+    safeAccount: any,
+    smartAccountClient:any,
+    pimlicoClient: any
+) => {
+    const trustAttestersAction = getTrustAttestersAction({
+        threshold: 1,
+        attesters: [
+          RHINESTONE_ATTESTER_ADDRESS, // Rhinestone Attester
+          MOCK_ATTESTER_ADDRESS, // Mock Attester - do not use in production
+        ],
+    })
+    console.log('***setTrustAttesters1', {trustAttestersAction})
+    const userOpHash1 = await smartAccountClient.sendUserOperation({
+        account: safeAccount,
+        calls: [
+            {
+            to: trustAttestersAction.target,
+            value: BigInt(0),
+            data: trustAttestersAction.callData,
+            },
+        ],
+    })
+    console.log('***setTrustAttesters2', {userOpHash1})
+    return pimlicoClient.waitForUserOperationReceipt({
+        hash: userOpHash1,
+    })
+} 
+
+export const installSmartSession = async (smartAccountClient:any, pimlicoClient: any, smartSessions:any) => {
+    console.log('***installSmartSession1', {smartSessions})
+    const opHash = await smartAccountClient.installModule(smartSessions)
+    console.log('***installSmartSession2', {opHash})
+    return pimlicoClient.waitForUserOperationReceipt({
+        hash: opHash,
+    })
+}
+
+export const getSession = (sessionOwner:any) => {  
+    const session: Session = {
+      sessionValidator: OWNABLE_VALIDATOR_ADDRESS,
+      sessionValidatorInitData: encodeValidationData({
+        threshold: 1,
+        owners: [sessionOwner.address],
+      }),
+      salt: toHex(toBytes("0", { size: 32 })),
+      userOpPolicies: [],
+      erc7739Policies: {
+        allowedERC7739Content: [],
+        erc1271Policies: [],
+      },
+      actions: [
+        {
+          actionTarget: "0x6fc7314c80849622b04d943a6714b05078ca2d05" as Address,
+          actionTargetSelector: toFunctionSelector("function increment()"),
+          actionPolicies: [getSudoPolicy()],
+        },
+      ],
+      chainId: BigInt(sepolia.id),
+    };
+    return session;
+};
+
+export const signSmartSession = async ( safeAccount: any, walletClient:any, session:any) => {
+    console.log('**signSmartSession0', {safeAccount, walletClient, 1:walletClient?.account.address});
+    const client = getPublicClient();
+    console.log('**signSmartSession1', {client, session});
+    const account = getAccount({
+      address: safeAccount.address,
+      type: 'safe',
+    })
+    console.log('**signSmartSession2', {account});
+    const sessionDetails = await getEnableSessionDetails({
+      sessions: [session],
+      account,
+      clients: [client],
+    })
+    console.log('**signSmartSession3', {sessionDetails});
+    const permissionEnableSig = await walletClient.signMessage({
+        message: { raw: sessionDetails.permissionEnableHash },
+    })
+    console.log('**signSmartSession4', {permissionEnableSig});
+    sessionDetails.enableSessionData.enableSession.permissionEnableSig = permissionEnableSig
+    return sessionDetails
+}
+
+export const createUserOperation = async (
+    sessionOwner: any,
+    safeAccount: any,
+    walletClient: any,
+    publicClient:any,
+    smartAccountClient: any,
+    smartSessions:any,
+    sessionDetails:any,
+    session:any
+) => {
+    const account = getAccount({
+        address: safeAccount.address,
+        type: 'safe',
+    })
+    console.log('***createUserOperation1', {
+        sessionOwner,
+        safeAccount,
+        walletClient,
+        publicClient,
+        smartAccountClient,
+        smartSessions,
+        sessionDetails    
+    })
+
+    const nonce = await getAccountNonce(publicClient, {
+        address: safeAccount.address,
+        entryPointAddress: entryPoint07Address,
+        key: encodeValidatorNonce({
+            account,
+            validator: smartSessions,
+        }),
+    })
+    console.log('***createUserOperation2',nonce)
+    const mockSignature = getOwnableValidatorMockSignature({
+        threshold: 1,
+    })
+    console.log('***createUserOperation3',mockSignature)
+    sessionDetails.signature = mockSignature
+    
+    const userOperation = await smartAccountClient.prepareUserOperation({
+        account: safeAccount,
+        calls: [
+            {
+            to: session.actions[0].actionTarget,
+            value: BigInt(0),
+            data: session.actions[0].actionTargetSelector,
+            },
+        ],
+        nonce,
+        signature: encodeSmartSessionSignature(sessionDetails),
+    })
+    console.log('***createUserOperation3',userOperation)
+    // https://docs.rhinestone.wtf/module-sdk/using-modules/smart-sessions#create-the-session-key-signature
+    
+    const userOpHashToSign = getUserOperationHash({
+        chainId: 11155111,
+        entryPointAddress: entryPoint07Address,
+        entryPointVersion: '0.7',
+        userOperation,
+    })
+    console.log('***createUserOperation4',userOpHashToSign)
+    const signature = await sessionOwner.signMessage({
+      message: { raw: userOpHashToSign },
+    })
+    sessionDetails.signature = signature
+    console.log('***createUserOperation5', signature)
+    userOperation.signature = encodeSmartSessionSignature(sessionDetails)
+    return userOperation
+}
+
+export const executeUserOperation = async (smartAccountClient:any, pimlicoClient:any, userOperation:any) => {
+    console.log('executeUserOperation**0', userOperation)
+    const userOpHash = await smartAccountClient.sendUserOperation(userOperation) 
+    console.log('executeUserOperation**1', userOpHash)
+    const receipt = await pimlicoClient.waitForUserOperationReceipt({
+        hash: userOpHash,
+    })
+    console.log('executeUserOperation**2', receipt)
+    return receipt
+}
